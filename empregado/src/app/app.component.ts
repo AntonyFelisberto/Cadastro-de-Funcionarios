0e7e7f28eac33e1a6d4f8d0b2b3c9175e1266588
Antony dos santos felisberto <antony.dos.santos.felisberto@gmail.com>
2022-09-24 16:29:25 -0300

    fazendo metodo de atualizar usuarios

--- empregado/src/app/app.component.ts
+++ empregado/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Empregado } from './empregado';
 import { EmpregadoService } from './empregado.service';
 
@@ -11,6 +12,7 @@ import { EmpregadoService } from './empregado.service';
 export class AppComponent implements OnInit{
   
   public empregado!: Empregado[];
+  public editarEmpregado!: Empregado;
 
   constructor(private empregadoService:EmpregadoService){}
  
@@ -27,6 +29,41 @@ export class AppComponent implements OnInit{
         alert(error.message);
       }
     )
+  }
+
+
+  public delEmpregados(): void {
+    this.empregadoService.getEmpregados().subscribe(
+      (response: Empregado[]) => {
+          this.empregado = response;
+      },
+      (error:HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
+
+  public onAddEmpregados(addForm: NgForm){
+    document.getElementById('add-employee-form')?.click();
+    this.empregadoService.addEmpregados(addForm.value).subscribe(
+      (response:Empregado) => {
+        console.log(response);
+        this.getEmpregados();
+      },(error: HttpErrorResponse) => {
+        alert(error.message)
+      }
+    );
+  }
+
+  public onUpdEmpregados(empregado: Empregado){
+    this.empregadoService.updateEmpregados(empregado).subscribe(
+      (response:Empregado) => {
+        console.log(response);
+        this.getEmpregados();
+      },(error: HttpErrorResponse) => {
+        alert(error.message)
+      }
+    );
   }
 
   public addEmpregado(){
@@ -53,10 +90,11 @@ export class AppComponent implements OnInit{
     button.setAttribute('data-toggle','modal');
 
     if(mode === 'edit'){
+      this.editarEmpregado = empregado;
       button.setAttribute('data-target','#updateEmpregadoModal');
     }
     if(mode === 'delete'){
-      button.setAttribute('data-target','#addEmpregadoModal');
+      button.setAttribute('data-target','#deleteEmployeeModal');
     }
     container?.appendChild(button);
     button.click();
